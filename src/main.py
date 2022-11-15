cca417f088741d1fc5ac3ca0e1a78b18d2712e9e
krblaz <blaz.kristan1@gmail.com>
2022-11-15 11:21:30 +0100

    Added function definitions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,38 @@
+from typing import List, Tuple
+
+
+class Simulation:
+    def __init__(self) -> None:
+        self.people = []
+        self.obstacles = []
+        self.tick_rate = 60
+        
+    def update():
+        pass
+    
+class Obstacle:
+    def __init__(self) -> None:
+        self.vertices: List[Tuple()] = []
+        
+class Person:
+    def __init__(self, position: Tuple[float,float], simulation: Simulation) -> None:
+        self.simulation = simulation
+        self.position: Tuple[float,float] = position
+        self.velocity: float = 0
+        self.angle: float = 0
+    
+    def loc_obstacle_avoiding(self): #Outputs speed and angle
+        pass
+    
+    def path_searching(self):
+        pass
+    
+    def goal_seeking(self):
+        pass
+        
+    def update(self):
+        pass
+        #Update position
+
 if __name__ == '__main__':
     print('entry point')
