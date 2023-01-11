6f8d4f22e2d44cc8c5ce71bfa7a399d04b207b5d
Philipp Temmel <philipp@temmel.me>
2023-01-11 21:06:50 +0100

    env update, extended github ReadMe documentation with description on how to run it, added three scenarios for video clip

--- README.md
+++ README.md
@@ -8,14 +8,28 @@ The public GIT repository can be found under: https://github.com/fairwell-coding
 
 The project is based on the article: Modeling of Crowd Evacuation With Assailants via a Fuzzy Logic Approach (Zhou, Dong, Wen, Yau and Sun, 2015)
 
-We will use the outlined fuzzy-logic approach in the paper as our starting point. 
+The concept outlined in this original paper establishes the basis for this fuzzy logic simulation.
 
-## Rough sketch w.r.t 3 report deadlines
+## Goals
 
-Our plan is to model a system, where a group of *n* pedestrians tries to escape a group of *m* assailants in a closed environment with randomly placed obstacles (a building or a maze). Our final goal would be to find an optimal survival strategy for the pedestrians. 
+The goal of this scientific project was to reconstruct a crowd simulation of pedestrians trying to evacuate to a common goal while avoiding each other, maneuver around any obstacles in their way and attackers/assailants attempting to catch as many pedestrians as possible before they reach their goal. 
 
-Initially, we would limit ourselves to only 1 assailant, modeling the environment ourselves and using an instruction set modeled on fuzzy logic to provide instructions for the pedestrians. Then, we would improve our model by increasing the number of assailants, building our model for a general closed environment, and implementing our pedestrians' survivability instinct using machine learning.
+The original paper only outlines the concepts but missed out on an Appendix, detailed and complete definition of membership functions, fuzzy logic rules etc which are reconstructed based on the author's intentions and extended by using appropriate definitions in order to provide a modular, easy to use and extendable simulation. 
 
-Specifically, for phase II and III we intend to explore suitable ML approaches based on genetic algorithms and compare the outcomes with the original fuzzy logic approach. Implementation will be based on either ML agents or our own project setup loading the final trained Pytorch models ourselves. We will also try to find suitable deep reinforcement approaches w.r.t. computational requirements. 
+## Levels reached
+
+Category I with all its subcategories and Category IV of the paper have been implemented in all their mentioned variants detailed as in the paper and extended whenever deemed necessary. Category I - part IV omits weighting and only applies an arithmetic mean instead. Category II and Category III are not vital to display the concept of having assailants in a crowd evacuation scenario and only reflect some fine-tuning. Thus, they have been omitted. 
+
+## How to run the simulation
+
+Running the simulation is straightforward. First a simulation instance is created, then a fluent builder API can be used to add the desired additional properties (pedestrians, assailants, obstacles) and at the end you simply call run() on the simulation object to start the simulation. That's it. Various different example scenarios can be found in simulation_tests.py. 
+
+Let's walk through the key options you might want to configure here:
+1. Simulation object. You need to provide a FuzzyDomains object. This object contains all membership functions for the fuzzy logic system. The whole system is modular/plug&play and thus any other set of membership functions with its corresponding domain objects (domains are class objects from the chosen underlying fuzzy logic library) can be defined and injected via constructor injection this way. A Goal object with its x and y coordinates has to be provided as well as the second parameter to the simulation constructor. By using the optional scale parameter you may zoom in/out (higher scale value zooms in tighter and vice versa). The optional parameter tick rate defines the speed at which the simulation is run. The tick rate denotes the number of simulation updates which are performed. Thus, a higher tick rate slows down the simulation speed. 
+2. add_obstacle() builder method available on the simulation object adds a new obstacle which pedestrians have to avoid. The constructor takes two tuples, i.e. x and y coordinates for each end point. 
+3. add_pedestrian() builder method available on the simulation object adds a new pedestrian. The first tuple denotes the x and y coordinates, the next parameter denotes the initial direction the pedestrian is looking at (0 degrees denotes looking to the right, i.e. x-axis oriented to the right denotes 0 degrees in the simulation), the third parameter denotes the initial movement speed of the pedestrian. 
+4. add_assailant() build method available on the simulation object adds a predator. The first parameters of its location coordinates, initial direction and speed are defined exactly as for pedestrians but additionally an optional paramater named 'predator_type' may be defined. By default a primitive predator object is defined which means that the predator will simply start to chase and attack the nearest pedestrian based on Euclidean distance. By definining 'predator_type="Advanced"' a more sophisticated version of a predator may be instantiated which will chase and eliminate the most isolated pedestrian based on its Euclidean distance to all other pedestrians in the simulation.
+5. run() on the simulation object finally runs the simulation in a pop-up window as configured. 
+
 
 
--- src/simulation_tests.py
+++ src/simulation_tests.py
@@ -28,25 +28,24 @@ def __test_assailant_target_selection():
     sim.add_obstacle([(3, -1), (3, -2), (3, -3)])
     sim.add_obstacle([(3, 0), (4, 1)])
     sim.add_pedestrian((2, 6), 180, 1.0)
-    #sim.add_assailant((2, -2), 180, 1.0)
-    #sim.add_assailant((5, 0), 180, 1.0)
+    # sim.add_assailant((2, -2), 180, 1.0)
+    # sim.add_assailant((5, 0), 180, 1.0)
     sim.run()
 
-
-    #for assailant in sim.get_assailants():
-        #print(assailant.target_selection_distance(key=min))
+    # for assailant in sim.get_assailants():
+    # print(assailant.target_selection_distance(key=min))
 
 
 def __test_assailant_target_selection_2():
     sim = Simulation(FuzzyDomains(), Goal((-16.34, -16.34)))
-    #sim.add_pedestrian((150, 155), 180, 1.0)
-    #sim.add_pedestrian((150, 124), 180, 1.0)
-    #sim.add_pedestrian((120, 165), 180, 1.0)
+    # sim.add_pedestrian((150, 155), 180, 1.0)
+    # sim.add_pedestrian((150, 124), 180, 1.0)
+    # sim.add_pedestrian((120, 165), 180, 1.0)
     sim.add_pedestrian((14, 57), 180, 1.0)
     sim.add_pedestrian((94, 37), 180, 1.0)
-    #sim.add_pedestrian((-31, 25), 180, 1.0)
-    #sim.add_pedestrian((111, 5), 180, 1.0)
-    #sim.add_pedestrian((-2, 5), 180, 1.0)
+    # sim.add_pedestrian((-31, 25), 180, 1.0)
+    # sim.add_pedestrian((111, 5), 180, 1.0)
+    # sim.add_pedestrian((-2, 5), 180, 1.0)
     sim.add_pedestrian((66, 56), 180, 1.0)
     sim.add_assailant((50, 75), 180, 1.0, predator_type="Advanced")
     sim.add_assailant((70, 35), 180, 1.0, predator_type="Advanced")
@@ -54,14 +53,15 @@ def __test_assailant_target_selection_2():
     sim.add_obstacle([(80, 80), (80, 30), (75, -9)])
     sim.run()
 
-    #for assailant in sim.get_assailants():
-        #print(assailant.target_selection_distance(key=min))
-        #print(assailant.target_selection_peripherality(key=max))
-        #print((assailant.coordinates[0], assailant.coordinates[1]))
+    # for assailant in sim.get_assailants():
+    # print(assailant.target_selection_distance(key=min))
+    # print(assailant.target_selection_peripherality(key=max))
+    # print((assailant.coordinates[0], assailant.coordinates[1]))
+
 
 def __test_simple_room():
     sim = Simulation(FuzzyDomains(), Goal((-16.34, -16.34)))
-    sim.add_obstacle([(20, 80),(20, 40)])
+    sim.add_obstacle([(20, 80), (20, 40)])
     sim.add_obstacle([(20, 100), (140, 100), (140, 20), (20, 20)])
     sim.add_pedestrian((90, 70), 180, 1.0)
     sim.add_pedestrian((100, 75), 180, 1.0)
@@ -69,20 +69,95 @@ def __test_simple_room():
     sim.add_pedestrian((50, 30), 180, 1.0)
     sim.add_pedestrian((40, 45), 180, 1.0)
     sim.add_pedestrian((60, 55), 180, 1.0)
-    sim.add_assailant((30, 30), 180, 1.0, predator_type = "Primitive")
-    sim.add_assailant((100, 32), 180, 1.0, predator_type = "Primitive")
-    #sim.add_pedestrian((66, 56), 180, 1.0)
-    #sim.add_pedestrian((66, 56), 180, 1.0)
-    #sim.add_pedestrian((66, 56), 180, 1.0)
+    sim.add_assailant((30, 30), 180, 1.0, predator_type="Primitive")
+    sim.add_assailant((100, 32), 180, 1.0, predator_type="Primitive")
+    # sim.add_pedestrian((66, 56), 180, 1.0)
+    # sim.add_pedestrian((66, 56), 180, 1.0)
+    # sim.add_pedestrian((66, 56), 180, 1.0)
     sim.run()
 
-    #for assailant in sim.get_assailants():
-        #print(assailant.target_selection_distance(key=min))
-        #print(assailant.target_selection_peripherality(key=max))
+    # for assailant in sim.get_assailants():
+    # print(assailant.target_selection_distance(key=min))
+    # print(assailant.target_selection_peripherality(key=max))
+
+
+def __test_video_scenario():
+    sim = Simulation(FuzzyDomains(), Goal((150, 150)), tick_rate=300)
+    sim.add_obstacle([(20, 80), (20, 40)])
+    sim.add_obstacle([(20, 100), (140, 100), (140, 20), (20, 20)])
+    sim.add_assailant((50, 50), 180, 1.0, predator_type="Primitive")
+    sim.add_assailant((120, 25), 180, 1.0, predator_type="Primitive")
+    sim.add_pedestrian((66, 56), 75, 1.0)
+    sim.add_pedestrian((68, 62), 75, 1.0)
+    sim.add_pedestrian((80, 78), 120, 1.0)
+    sim.add_pedestrian((110, 80), 120, 1.0)
+    sim.add_pedestrian((100, 40), 120, 1.0)
+    sim.add_pedestrian((120, 110), 120, 1.0)
+    sim.run()
+
+
+def __test_video_scenario2():
+    """ Pedestrians navigating obstacle to get to goal.
+    """
+
+    sim = Simulation(FuzzyDomains(), Goal((50, 50)), tick_rate=300)
+    sim.add_obstacle([(10, 70), (100, 70)])
+    sim.add_pedestrian((40, 85), 75, 1.0)
+    sim.add_pedestrian((40, 90), 135, 1.0)
+    sim.add_pedestrian((45, 80), 135, 1.0)
+    sim.run()
+
+
+def __test_video_scenario2():
+    """ Pedestrians navigating obstacle to get to goal.
+    """
+
+    sim = Simulation(FuzzyDomains(), Goal((50, 50)), tick_rate=300)
+    sim.add_obstacle([(10, 70), (100, 70)])
+
+    # pedestrian group 1
+    sim.add_pedestrian((40, 85), 75, 1.0)
+    sim.add_pedestrian((40, 90), 135, 1.0)
+    sim.add_pedestrian((45, 80), 135, 1.0)
+
+    # pedestrian group 2
+    sim.add_pedestrian((80, 85), 75, 1.0)
+    sim.add_pedestrian((80, 90), 30, 1.0)
+    sim.add_pedestrian((85, 80), 210, 1.0)
+
+    sim.run()
+
+
+def __test_video_scenario3():
+    """ Showcase difference in primitive vs advanced predator behaviour. The primitive predator goes for the nearest pedestrian while the advanced predator goes for the isolated one.
+    """
+
+    sim = Simulation(FuzzyDomains(), Goal((50, 50)), tick_rate=1000)
+    sim.add_obstacle([(10, 70), (100, 70)])
+
+    # pedestrian group 1
+    sim.add_pedestrian((40, 85), 75, 1.0)
+    sim.add_pedestrian((40, 90), 135, 1.0)
+    sim.add_pedestrian((45, 80), 135, 1.0)
+
+    # primitive predator goes for group 1 since these pedestrians are nearer
+    sim.add_assailant((60, 100), 180, 1.0, predator_type="Primitive")
+
+    # pedestrian group 2 -> "isolated victim"
+    sim.add_pedestrian((80, 85), 75, 1.0)
+
+    # advanced predator goes for group 2 since group 2 is isolated
+    sim.add_assailant((50, 100), 180, 1.0, predator_type="Advanced")
+
+    sim.run()
+
 
 if __name__ == '__main__':
-    #__test_goal_angle()
+    # __test_goal_angle()
     # __test_goal_angle_2()
-    #__test_assailant_target_selection()
-    #__test_assailant_target_selection_2()
-    __test_simple_room()
+    # __test_assailant_target_selection()
+    # __test_assailant_target_selection_2()
+    # __test_simple_room()
+    # __test_video_scenario()
+    # __test_video_scenario2()
+    __test_video_scenario3()
